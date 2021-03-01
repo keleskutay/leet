1b21c4471fe8693e24f84efa65a029b2126aeb3d
Kutay <kutaykeless@gmail.com>
2021-03-01 20:42:27 +0300

    Düzenleme

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -3,17 +3,18 @@
  * @param {number[]} nums2
  * @return {number}
  */
+//Kutay KELEŞ 3/1/2021 /// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
 
 var findMedianSortedArrays = function(nums1, nums2) {
   let nums3 = [...nums1,...nums2].sort(function(a,b) {return b-a;});
   if(nums3.length % 2 == 1){
-  	console.log(nums3[parseInt(nums3.length / 2)].toFixed(5))
+  	return(nums3[parseInt(nums3.length / 2)].toFixed(5))
   }
   else{
-  	console.log(((nums3[parseInt(nums3.length/2)] + nums3[parseInt(nums3.length/2) -1 ]) / 2) .toFixed(5))
+  	return(((nums3[parseInt(nums3.length/2)] + nums3[parseInt(nums3.length/2) -1 ]) / 2) .toFixed(5))
   }
 
 };
 
-findMedianSortedArrays([3],[-2,-1])
 
